1fa2e3363cfea2e6ffa04f045b044bf6f96dc8d5
skypekitten9 <github_.3w93q@dralias.com>
2025-09-01 09:45:16 +0200

    improve accessability

--- src/components/list/list.tsx
+++ src/components/list/list.tsx
@@ -3,14 +3,14 @@ import "./list.css";
 type TList = {
   title: string;
   items: string[];
-};
+} & React.HTMLAttributes<HTMLDivElement>;
 
-export function List({ title, items }: TList) {
+export function List({ title, items, ...rest }: TList) {
   const listItemElements = items.map((item) => (
     <li className="listItem">{item}</li>
   ));
   return (
-    <div className="listContainer">
+    <div className="listContainer" {...rest}>
       <h2 className="listTitle">{title}</h2>
       <ul>{...listItemElements}</ul>
     </div>
--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -4,9 +4,13 @@ import { List } from "../list/list";
 import "./main.css";
 import { Banner } from "../banner/banner";
 
+const MIN_REQUIRED_INGRIDIENTS: number = 4;
+
 export function Main() {
   const [ingredientList, setIngredientList] = useState<string[]>([]);
   const shouldRenderIgredientList = ingredientList.length > 0;
+  const shouldRenderGetRecipeBanner =
+    ingredientList.length >= MIN_REQUIRED_INGRIDIENTS;
   const handleOnIngredientSubmited = (ingredient: string) => {
     const lowercase = ingredient.toLowerCase();
     const firstCapitalized =
@@ -19,20 +23,26 @@ export function Main() {
 
   return (
     <main>
-      <div className="main-content">
+      <section className="main-section">
         <IngredientForm onIngredientSubmited={handleOnIngredientSubmited} />
         {shouldRenderIgredientList && (
           <>
-            <List title="Ingridents on hand:" items={ingredientList} />
-            <Banner
-              title={"Ready for a recipe?"}
-              description={"Generate a recipe from your list of ingredients."}
-            >
-              <button className="get-recipe-button">Get a recipe</button>
-            </Banner>
+            <List
+              title="Ingridents on hand:"
+              items={ingredientList}
+              aria-label=""
+            />
+            {shouldRenderGetRecipeBanner && (
+              <Banner
+                title={"Ready for a recipe?"}
+                description={"Generate a recipe from your list of ingredients."}
+              >
+                <button className="get-recipe-button">Get a recipe</button>
+              </Banner>
+            )}
           </>
         )}
-      </div>
+      </section>
     </main>
   );
 }
