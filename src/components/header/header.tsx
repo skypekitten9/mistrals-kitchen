08c06dea4e2140175facde4a68e27ea33d75c64d
Victor Enhörning <github_.3w93q@dralias.com>
2025-12-08 19:48:18 +0100

    update icon and header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,11 +1,12 @@
 import "./header.css";
-import ChefLogo from "../../assets/chef-claude.png";
+import ChefLogo from "../../../public/chef.svg";
 
 export function Header() {
   return (
     <header>
-      <img width={43} height={52} src={ChefLogo} alt="chef claude logo"></img>
-      <h1 className="page-title">Chef Claude</h1>
+      <h1 className="page-title">Mistral's</h1>
+      <img  src={ChefLogo} alt="chef logo"></img>
+      <h1 className="page-title">Kitchen</h1>
     </header>
   );
 }
