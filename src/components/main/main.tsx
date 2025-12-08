c023d394cba03cee964190f3cd6996f6082c1e0a
Victor Enhörning <github_.3w93q@dralias.com>
2025-12-08 19:24:29 +0100

    improve error handling

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -10,10 +10,17 @@ import {
 } from "../sections/recipe-section";
 import { getRecipeFromAi } from "../../external";
 
+const RecipeState = {
+  None: 0,
+  Loading: 1,
+  Error: 2,
+  Loaded: 3,
+} as const;
+type RecipeState = typeof RecipeState[keyof typeof RecipeState];
+
 export function Main() {
+  const [recipeState, setRecipeState] = useState<RecipeState>(RecipeState.None);
   const [recipe, setRecipe] = useState<string | undefined>(undefined);
-  const [recipeLoading, setRecipeLoading] = useState(false);
-  const [recipeError, setRecipeError] = useState(false);
   const recipeRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -23,24 +30,19 @@ export function Main() {
   }, [recipe]);
 
   const handleOnGetRecipie: TOnGetRecipe = (ingridients) => {
-    setRecipeLoading(true);
+    setRecipeState(RecipeState.Loading);
     getRecipeFromAi(ingridients).then((recipe) => {
-      if (recipe) {
-        setRecipeError(false);
-      } else {
-        setRecipeError(true);
-      }
       setRecipe(recipe);
-      setRecipeLoading(false);
+      setRecipeState(()=>recipe ? RecipeState.Loaded : RecipeState.Error);
     });
   };
 
   return (
     <main>
       <IngredientSection onGetRecipe={handleOnGetRecipie} />
-      {recipe && <RecipeSection ref={recipeRef} recipe={recipe} />}
-      {recipeLoading && <RecipeSectionSkeletonLoader />}
-      {recipeError && (
+      {(recipe && recipeState === RecipeState.Loaded) && <RecipeSection ref={recipeRef} recipe={recipe} />}
+      {recipeState === RecipeState.Loading && <RecipeSectionSkeletonLoader />}
+      {recipeState === RecipeState.Error && (
         <h3>Seems like the chef is on vacation, try again later.</h3>
       )}
     </main>
